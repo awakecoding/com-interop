5ea2188d3fa9224a7e1e1a7af7d59cb7dc78fff0
Marc-André Moreau <mamoreau@devolutions.net>
2022-01-11 15:40:56 -0500

    create monkey

--- rust/sample/src/lib.rs
+++ rust/sample/src/lib.rs
@@ -1,29 +1,7 @@
 
-use com::interfaces::{IUnknown,IClassFactory};
-
-com::interfaces! {
-    #[uuid("b05271fc-4a1d-45ae-8f7a-c5f2dc0cde88")]
-    pub unsafe interface IBike : IUnknown {
-        pub fn ride(&self);
-    }
-
-    #[uuid("2015c764-184c-4f50-86cf-a00e831658e1")]
-    pub unsafe interface ICar : IUnknown {
-        pub fn drive(&self);
-    }
-}
-
-com::class! {
-    pub class HybridVehicle : IBike, ICar {}
-
-    impl IBike for HybridVehicle {
-        fn ride(&self) {}
-    }
-
-    impl ICar for HybridVehicle {
-        fn drive(&self) {}
-    }
-}
+use com::interfaces::{IUnknown};
+use core::ffi::c_void;
+use std::ptr;
 
 com::interfaces! {
     #[uuid("EFF8970E-C50F-45E0-9284-291CE5A6F771")]
@@ -43,6 +21,13 @@ com::class! {
 }
 
 #[no_mangle]
-pub extern fn hello_com() -> *const u8 {
-    "Hello, world!\0".as_ptr()
+pub extern fn create_monkey() -> *mut c_void {
+    unsafe {
+        let instance = Monkey::allocate();
+        let interface_handle = instance.query_interface::<IAnimal>().unwrap();
+        interface_handle.AddRef();
+        let mut pv_object: *mut c_void = ptr::null_mut();
+        interface_handle.QueryInterface(&IID_IANIMAL, &mut pv_object);
+        return pv_object;
+    }
 }
